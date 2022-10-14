c2fa710d8315df9b3c42f3bffdf9c6febc39b9b5
ingrid-oguro <104454935+ingrid-oguro@users.noreply.github.com>
2022-10-14 08:34:23 -0300

    Update appst.py

--- appst.py
+++ appst.py
@@ -9,11 +9,11 @@ pip.main(["install", "openpyxl"])
 
 #Para importar a base de dados (obs: extrair caminho da pasta do colab com botão direito do mouse) e
 #remover as linhas que têm espaço vazio (e.g., notas ainda não lançadas):
-base = pd.read_excel('/Base 13102022v.xlsx')
+base = pd.read_excel('Base 13102022v.xlsx')
 base.dropna(inplace=True)
 
 #Para importar a base de dados sem remover as linhas que têm espaço vazio:
-base0 = pd.read_excel('/Base 13102022v.xlsx')
+base0 = pd.read_excel('Base 13102022v.xlsx')
 
 #Para definir as configurações da aba e da página:
 PAGE_CONFIG = {"page_title": "ESEG - Apoio ao aluno", "page_icon": ":globe_with_meridians:", "layout": "wide"}
